01d66ea930d138a575e923c08e74ced8bc48b37f
Brayan <brayansachez67@hotmail.com>
2022-04-15 12:06:13 -0500

    improvemets

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ intents.messages = True
 miguel = "日日南Breast Apostle"
 aiz = "Kurisu-tina / 日日南BreastNoble"
 
+knanPJ = 日南
+knan = Canan
+
+kknanPj = 日日南
+kknan = Cacanan
+
 aizDesbasado = "Kurisu-tina / 日南BreastNoble"
 miguelDesbasado = "日南Breast Apostle"
 
 bot = discord.Client(intents=intents)
 @bot.event
 async def on_member_update(before, after):
-    if(after.nick == aizDesbasado ):
+    if(after.nick in knan):
+        temp2 = after.nick.replace(knan,kknan)
         time.sleep(3)
-        await after.edit(nick= aiz)
+        await after.edit(nick= temp2)
 
-    if(after.nick == miguelDesbasado):
-        time.sleep(3)
-        await after.edit(nick= miguel)
+    if(after.nick in kknanPJ):
+        temp =  after.nick.replace(knanPJ, kknanPJ)
+        time.sleep(3)   
+        await after.edit(nick= temp)
 
 
 
