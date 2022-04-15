22ff05f0db8584f319fd3bc96945b73d6e28ced5
Brayan <brayansachez67@hotmail.com>
2022-04-15 10:52:41 -0500

    nueva version

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
+import os
 import discord
 import time
-from decouple import config
-
+my_secret = os.environ['TOKEN']
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
@@ -9,17 +9,20 @@ intents.messages = True
 miguel = "日日南Breast Apostle"
 aiz = "Kurisu-tina / 日日南BreastNoble"
 
+aizDesbasado = "Kurisu-tina / 日南BreastNoble"
+miguelDesbasado = "日南Breast Apostle"
+
 bot = discord.Client(intents=intents)
 @bot.event
 async def on_member_update(before, after):
-    if(after.nick == "Kurisu-tina / 日南BreastNoble" ):
-        time.sleep(10)
+    if(after.nick == aizDesbasado ):
+        time.sleep(3)
         await after.edit(nick= aiz)
 
-    if(after.nick == "日南Breast Apostle"):
-        time.sleep(10)
+    if(after.nick == miguelDesbasado):
+        time.sleep(3)
         await after.edit(nick= miguel)
 
 
 
-bot.run(config('TOKEN'))
+bot.run(my_secret)
