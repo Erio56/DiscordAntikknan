657a3d8a639ef69665e16d89656d620ddf2fa677
Brayan <brayansachez67@hotmail.com>
2022-04-15 20:16:19 -0500

    working

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 
-listaDesbasadaM = ["日南BreastApostle","日 南Breast Apostle", "日'南BreastApostle", "日-南BreastApostle" ]
+listaDesbasadaM = ["日南BreastApostle","日 南Breast Apostle", "日'南BreastApostle", "日-南BreastApostle", "日''南Breast Apostle" ]
 ListaDesbasadaA = ["Kurisu-tina/日南BreastNoble", "Kurisu-tina / 日 南BreastNoble"]
 
 miguel = "日日南Breast Apostle"
@@ -20,6 +20,7 @@ bot = discord.Client(intents=intents)
 async def on_member_update(before, after):
     temp = after.nick
     trimed = temp.replace(' ', '')
+    trimed = temp.replace("'", '')
     print(trimed)
     for i in range (len(ListaDesbasadaA)):
         if(trimed == ListaDesbasadaA[i]):
