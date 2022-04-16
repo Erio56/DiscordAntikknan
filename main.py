ec58d9ca3e2c4c8b20a457023212080171190003
Brayan <brayansachez67@hotmail.com>
2022-04-15 21:18:14 -0500

    xi

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import discord
 import time
 import string
-my_secret = os.environ['TOKEN']
+#my_secret = os.environ['TOKEN']
+token = "OTY0MzE4NjE4NzIyNjQ0MDIw.Yli5sg.6v0IFFrLsdNmioyiM-pIvFmkM9Q"
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
 
-listaDesbasadaM = ["日南BreastApostle","日 南Breast Apostle", "日'南BreastApostle", "日-南BreastApostle", "日''南Breast Apostle", "Hí南BreastApostle", "Hi南Breast Apostle"]
-ListaDesbasadaA = ["Kurisu-tina/日南BreastNoble", "Kurisu-tina / 日 南BreastNoble"]
+#listaDesbasadaM = ["日南BreastApostle","日 南Breast Apostle", "日'南BreastApostle", "日-南BreastApostle", "日''南Breast Apostle", "Hí南BreastApostle", "Hi南Breast Apostle"]
+#ListaDesbasadaA = ["Kurisu-tina/日南BreastNoble", "Kurisu-tina / 日 南BreastNoble"]
 
 miguel = "KK南Breast Apostle"
 aiz = "Kurisu-tina / KK南BreastNoble"
@@ -19,19 +20,17 @@ aiz = "Kurisu-tina / KK南BreastNoble"
 bot = discord.Client(intents=intents)
 @bot.event
 async def on_member_update(before, after):
-    temp = after.nick
-    trimed = temp.translate(str.maketrans("", "",string.whitespace))
-    trimed = trimed.translate(str.maketrans("", "","'"))
-    trimed = trimed.translate(str.maketrans("", "",'*'))
-    print(trimed)
-    for i in range (len(ListaDesbasadaA)):
-        if(trimed == ListaDesbasadaA[i]):
-            print(ListaDesbasadaA[i])
-            await after.edit(nick= aiz)
-    for e in range(len(listaDesbasadaM)):
-        if(trimed == listaDesbasadaM[e]):
-            print(listaDesbasadaM[e])
-            await after.edit(nick= miguel)
+    #temp = after.nick
+    #trimed = temp.translate(str.maketrans("", "",string.whitespace))
+    #trimed = trimed.translate(str.maketrans("", "","'"))
+    #trimed = trimed.translate(str.maketrans("", "",'*'))
+    #print(trimed)
+    print(after.id)
+    #if(after.id == bot)
+    #    await after.edit(nick= aiz)
+
+    #if(trimed != miguel):
+    #    await after.edit(nick= miguel)
 
 
-bot.run(my_secret)
+bot.run(token)
